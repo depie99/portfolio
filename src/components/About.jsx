766c30b7cae2d97c5f7a574c3ecc35850e6900bf
depie99 <detectivepineapple99@gmail.com>
2023-12-07 10:28:11 +0330

    adding a function

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 
 const About = () => {
+    const shoot = () => {
+        alert("Great Shot!");
+    }
     return (
         <div
             name="about"
             id="about"
             className="w-full h-screen bg-[#0a192f] text-gray-300"
         >
+
             <div className="flex flex-col justify-center items-center w-full h-full">
                 <div className=" py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
                     <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
                         <div className="sm:text-right pb-8 pl-4">
-                            <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
+                            <p id='add' className="text-4xl font-bold inline border-b-4 border-cyan-500">
                                 About
                             </p>
+                            <button className="mx-2 border-2 rounded px-2" onClick={shoot}>
+                                Take the shot!
+                            </button>
+
                         </div>
                         <div></div>
                     </div>
